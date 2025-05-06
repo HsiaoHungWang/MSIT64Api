fc67e67e06e1086fa272c0999bc87dc02e18def9
User <User@MSITXX-00>
2025-05-06 10:41:50 +0800

    加入專案檔案。

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MSIT64Api.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string? CategoryName { get; set; }
+}
---
+++ Models/SpotImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MSIT64Api.Models;
+
+public partial class SpotImage
+{
+    public int ImageId { get; set; }
+
+    public int? SpotId { get; set; }
+
+    public string? ImageTitle { get; set; }
+
+    public string? ImagePath { get; set; }
+}
